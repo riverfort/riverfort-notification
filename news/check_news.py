6a7efccf84a895b14f0482c8960e4b954654e786
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-30 13:12:43 +0100

    Regress

--- news/check_news.py
+++
@@ -1,8 +0,0 @@
-from database.configuration.config import DatabaseConnection
-
-
-database = DatabaseConnection(
-    "riverfort_notification", "riverfort", "rgctechnology", "localhost", 5432
-)
-
-uk_news_url = "https://www.investegate.co.uk/Rss.aspx?company="
