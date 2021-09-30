e38ea70260cc745a4a92c1f0a0aa2d4688204ed0
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-30 08:59:37 +0100

    Update setup_companies

--- database/setup_companies.py
+++ database/setup_companies.py
@@ -13,7 +13,9 @@ with open("/Users/nieqiuyang/Desktop/lse_shares.csv", "r") as f:
         database.insert_data(
             """
             INSERT INTO companies (company_symbol, company_name, exchange) 
-            VALUES (%s, %s, %s) 
+            VALUES (%s, %s, %s)
+            ON CONFLICT (company_symbol) DO UPDATE SET
+            (company_symbol, company_name, exchange) = (EXCLUDED.company_symbol, EXCLUDED.company_name, EXCLUDED.exchange)
             RETURNING company_symbol
             """,
             (row[0], row[1], "LSE"),
