4a8366bf580883b3b9fe78b5cecaf9b80157e42f
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-01 09:00:59 +0100

    Refactor

--- core/company_news.py
+++ core/company_news.py
@@ -1,36 +1,8 @@
 from db_config.connection import DatabaseConnection
-import feedparser
 
-uk_news_url = "https://www.investegate.co.uk/Rss.aspx?company="
 database = DatabaseConnection(
     "riverfort_notification", "riverfort", "rgctechnology", "localhost", 5432
 )
-
-
-def check_company_latest_news():
-    company_symbols = fetch_watchlist_companies()
-    for company_symbol in company_symbols:
-        fetch_company_latest_news(company_symbol)
-
-
-def fetch_watchlist_companies():
-    company_symbols = database.select_data(
-        "SELECT DISTINCT company_symbol FROM watchlist"
-    )
-    return [i[0] for i in company_symbols]
-
-
-def fetch_company_latest_news(company_symbol):
-    lateset_feed = feedparser.parse(uk_news_url + company_symbol)["entries"][:1][0]
-    title = lateset_feed["title"]
-    pub_date = lateset_feed["published"]
-    link = lateset_feed["link"]
-    if is_latest_news_in_db(company_symbol, pub_date):
-        print("no news - " + company_symbol)
-    else:
-        print("breaking news! - " + company_symbol)
-        delete_recent_company_news(company_symbol)
-        add_latest_news_to_db(company_symbol, pub_date, title)
 
 
 def is_latest_news_in_db(company_symbol, pub_date):
@@ -55,6 +27,3 @@ def delete_recent_company_news(company_symbol):
     database.delete(
         "DELETE FROM company_news WHERE company_symbol=%s", (company_symbol,)
     )
-
-
-check_company_latest_news()
--- core/notification.py
+++ core/notification.py
@@ -1,5 +1,12 @@
 from db_config.connection import DatabaseConnection
+from company_news import (
+    is_latest_news_in_db,
+    delete_recent_company_news,
+    add_latest_news_to_db,
+)
+import feedparser
 
+uk_news_url = "https://www.investegate.co.uk/Rss.aspx?company="
 database = DatabaseConnection(
     "riverfort_notification", "riverfort", "rgctechnology", "localhost", 5432
 )
@@ -16,6 +23,32 @@ def fetch_all_device_tokens_of(company_symbol):
         "SELECT device_token FROM watchlist WHERE company_symbol=%s", (company_symbol,)
     )
     return [i[0] for i in device_tokens]
+
+
+def check_company_latest_news():
+    company_symbols = fetch_watchlist_companies()
+    for company_symbol in company_symbols:
+        fetch_company_latest_news(company_symbol)
+
+
+def fetch_watchlist_companies():
+    company_symbols = database.select_data(
+        "SELECT DISTINCT company_symbol FROM watchlist"
+    )
+    return [i[0] for i in company_symbols]
+
+
+def fetch_company_latest_news(company_symbol):
+    lateset_feed = feedparser.parse(uk_news_url + company_symbol)["entries"][:1][0]
+    title = lateset_feed["title"]
+    pub_date = lateset_feed["published"]
+    link = lateset_feed["link"]
+    if is_latest_news_in_db(company_symbol, pub_date):
+        print("no news - " + company_symbol)
+    else:
+        print("breaking news! - " + company_symbol)
+        delete_recent_company_news(company_symbol)
+        add_latest_news_to_db(company_symbol, pub_date, title)
 
 
 send_push_notification()
