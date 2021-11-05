12d38ee6554c5c948072468701f9540bb15c1bfd
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-05 13:07:32 +0000

    Add db

---
+++ db/db.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+
+class DB:
+    def __init__(self, host, port, database, user, password):
+        self._con = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password,
+        )
+
+    def close(self):
+        self._con.close()
+
+    def get(self, sql):
+        cur = self._con.cursor()
+        cur.execute(sql)
+        result = cur.fetchall()
+        cur.close
+        return result
+
+    def write(self, sql):
+        cur = self._con.cursor()
+        cur.execute(sql)
+        self._con.commit()
+        cur.close
