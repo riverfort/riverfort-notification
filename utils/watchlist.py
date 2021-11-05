e8376d9f416fe7eb487820e16a6564f8ea6a8f82
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-05 17:08:20 +0000

    Add utils

---
+++ utils/watchlist.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+db = os.path.dirname(currentdir)
+sys.path.insert(0, db)
+
+from db.db.db import DB
+
+db = DB(
+    host="localhost",
+    port=5432,
+    database="riverfort_notification",
+    user="riverfort",
+    password="rgctechnology",
+)
+
+
+def get_device_tokens_of(company_symbol):
+    device_tokens = db.get(
+        "SELECT device_token FROM watchlist WHERE company_symbol=%s", company_symbol
+    )
+    result = [device_token[0] for device_token in device_tokens]
+    return result
+
+
+def get_companies():
+    companies = db.get("SELECT DISTINCT company_symbol FROM watchlist")
+    result = [company[0] for company in companies]
+    return result
+
+
+result = get_companies()
+print(result)
