9be17c105d39000f86115c29db87e63e57bca2ae
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-05 10:27:47 +0000

    Add fetch_company_symbols

--- core/companies.py
+++ core/companies.py
@@ -10,3 +10,14 @@ def fetch_exchange_of_company(company_symbol):
         "SELECT exchange FROM companies WHERE company_symbol=%s", (company_symbol,)
     )
     return [i[0] for i in exchange][0]
+
+
+def fetch_company_symbols():
+    company_symbols = database.select_data("SELECT company_symbol FROM companies")
+    symbols = [i[0] for i in company_symbols]
+    symbol_list = list(map(lambda symbol: symbol + ".L", symbols))
+    result = ",".join(symbol_list)
+    print(result)
+
+
+fetch_company_symbols()
