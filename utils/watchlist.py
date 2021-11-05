d98450f07ca8752535e82cb558778af27888aadd
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-05 17:16:48 +0000

    Create utils package

--- utils/watchlist.py
+++ utils/watchlist.py
@@ -1,23 +1,4 @@
-import os
-import sys
-import inspect
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-db = os.path.dirname(currentdir)
-sys.path.insert(0, db)
-
-from db.db.conn import Conn
-
-conn = Conn(
-    host="localhost",
-    port=5432,
-    database="riverfort_notification",
-    user="riverfort",
-    password="rgctechnology",
-)
-
-
-def get_device_tokens_of(company_symbol):
+def get_device_tokens_of(conn, company_symbol):
     device_tokens = conn.get(
         "SELECT device_token FROM watchlist WHERE company_symbol=%s", company_symbol
     )
@@ -25,11 +6,7 @@ def get_device_tokens_of(company_symbol):
     return result
 
 
-def get_companies():
+def get_companies(conn):
     companies = conn.get("SELECT DISTINCT company_symbol FROM watchlist")
     result = [company[0] for company in companies]
     return result
-
-
-result = get_companies()
-print(result)
