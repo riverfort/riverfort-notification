9368e111a509764a1e3cbad76e7f569ec8c9c854
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-06 15:55:39 +0000

    Update auth_key path

--- apns/apns.py
+++ apns/apns.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from aioapns import APNs, NotificationRequest, PushType
 
 apns = APNs(
-    key="auth_key/AuthKey_58M45W44RZ.p8",
+    key="apns/auth_key/AuthKey_58M45W44RZ.p8",
     key_id="58M45W44RZ",
     team_id="A8X2W38P62",
     topic="com.RiverFort",  # Bundle ID
