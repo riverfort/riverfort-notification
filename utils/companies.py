f7efc2b7d7e3f7a94f90905da8839015462b4752
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-06 12:21:01 +0000

    Add companies module

---
+++ utils/companies.py
@@ -0,0 +1,11 @@
+def get_company_symbols(conn):
+    symbols = conn.get("SELECT company_symbol FROM companies")
+    result = [symbol[0] for symbol in symbols]
+    return result
+
+
+def get_companys_exchange(conn, company_symbol):
+    exchange = conn.get(
+        "SELECT exchange FROM companies WHERE company_symbol=%s", company_symbol
+    )
+    return exchange[0][0]
