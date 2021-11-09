a1b12101b9ab5e15a7f4e2e340f712ef01b0209b
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-09 09:22:33 +0000

    Update type for change_percent

--- db/create_schema.py
+++ db/create_schema.py
@@ -44,7 +44,7 @@ conn.write(
     company_symbol VARCHAR(200) REFERENCES companies (company_symbol),
     price NUMERIC,
     change NUMERIC,
-    change_percent NUMERIC,
+    change_percent VARCHAR(200),
     PRIMARY KEY (company_symbol))
     """
 )
