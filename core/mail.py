ef2f82b65bef84c91469493a06179ebae35832e0
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-10 13:55:37 +0000

    Add conn to main

--- core/mail.py
+++ core/mail.py
@@ -6,8 +6,20 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
+from db.db.conn import Conn
+from utils.company_quotes import get_top_gain_company_quotes
 from mail.template_render import render_template
 from mail.mail import send_email
+
+conn = Conn(
+    host="localhost",
+    port=5432,
+    database="riverfort_notification",
+    user="riverfort",
+    password="rgctechnology",
+)
+
+company_quotes = get_top_gain_company_quotes(conn=conn)
 
 
 item1 = "hello"
@@ -16,3 +28,5 @@ item2 = "world"
 contacts = ["tech@riverfortcapital.com", "qyang.nie@gmail.com"]
 html = render_template("mail/templates/template.j2", **locals())
 send_email(subject="Top Gainers: London", contacts=contacts, content="Hello", html=html)
+
+conn.close()
