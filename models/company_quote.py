2ec5c8e11b92146da6f333d44526b4d49141ac45
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-11 10:11:38 +0000

    Add company_name attribute

--- models/company_quote.py
+++ models/company_quote.py
@@ -1,7 +1,8 @@
 class CompanyQuote:
-    def __init__(self, company_symbol, price, change, change_percent, market_time):
+    def __init__(self, company_symbol, price, change, change_percent, market_time, company_name=None):
         super().__init__()
         self.company_symbol = company_symbol
+        self.company_name = company_name
         self.price = price
         self.change = change
         self.change_percent = change_percent
@@ -10,6 +11,7 @@ class CompanyQuote:
     def __str__(self):
         return f"""CompanyQuote: 
         - company_symbol: {self.company_symbol}
+        - company_name: {self.company_name}
         - price: {self.price}
         - change: {self.change}
         - change_percent: {self.change_percent}
--- utils/company_quotes.py
+++ utils/company_quotes.py
@@ -12,7 +12,7 @@ from models.company_quote import CompanyQuote
 def get_top_gain_company_quotes(conn) -> CompanyQuote:
     company_quotes = conn.get(
         """
-        SELECT CQS.company_symbol, price, change, change_percent, TO_TIMESTAMP(market_time)
+        SELECT CQS.company_symbol, CS.company_name, price, change, change_percent, TO_TIMESTAMP(market_time)
         FROM company_quotes CQS INNER JOIN companies CS ON CQS.company_symbol=CS.company_symbol
         WHERE change_percent IS NOT NULL 
         AND TO_TIMESTAMP(market_time)::DATE=CURRENT_DATE 
@@ -23,10 +23,11 @@ def get_top_gain_company_quotes(conn) -> CompanyQuote:
         map(
             lambda company_quote: CompanyQuote(
                 company_symbol=company_quote[0],
-                price=company_quote[1],
-                change=company_quote[2],
-                change_percent=company_quote[3],
-                market_time=company_quote[4],
+                company_name=company_quote[1],
+                price=company_quote[2],
+                change=company_quote[3],
+                change_percent=company_quote[4],
+                market_time=company_quote[5],
             ),
             company_quotes,
         )
